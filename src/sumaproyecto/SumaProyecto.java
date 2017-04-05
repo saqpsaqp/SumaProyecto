97a684239ce8ac323784384e2a740b59c1efa4ab
Saul <saqpsaqp@gmail.com>
2017-04-05 08:54:58 -0400

    Commit Inicial

---
+++ src/sumaproyecto/SumaProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * SumaProyecto
+ * ------------
+ * Ejemplo para el aprendizaje de Git
+ * Programación I - UNET
+ * Prof. Saúl Quintero
+ */
+package sumaproyecto;
+
+public class SumaProyecto {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
