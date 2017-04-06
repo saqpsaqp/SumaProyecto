5f42e04038931168b4cc21c60dd72f6d49108354
Saul <saqpsaqp@gmail.com>
2017-04-06 08:49:56 -0400

    metodo double Sumar

--- src/sumaproyecto/Suma.java
+++ src/sumaproyecto/Suma.java
@@ -3,11 +3,18 @@ package sumaproyecto;
 public class Suma {
     
     public static int Sumar(int ... nums){
-        int acum=0;
+        int acum1=0;
         for (int num: nums){
+            acum1+=num;
+        }
+        return acum1;
+    }
+    
+    public static double Sumar(double ... nums){
+        double acum=0;
+        for (double num: nums){
             acum+=num;
         }
         return acum;
-    }
-    
+    }   
 }
